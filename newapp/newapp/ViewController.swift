8237555d217a60e3dabe654d4bff991e44adf104
Влад <vladyslav.andrus@gmail.com>
2019-07-12 15:17:20 +0200

    innitial xcode project

---
+++ newapp/newapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newapp
+//
+//  Created by Влад on 7/12/19.
+//  Copyright © 2019 Влад. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
