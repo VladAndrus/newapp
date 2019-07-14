2f3e8355b6993a18f250438a09b0f1d5695f1032
Влад <vladyslav.andrus@gmail.com>
2019-07-14 16:48:39 +0200

    ipad sizes

--- newapp/newapp/ViewController.swift
+++ newapp/newapp/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        logo.layer.borderColor = UIColor.black.cgColor
+        logo.layer.borderWidth = 2.0
+        logo.layer.cornerRadius = 2.0
     }
 
 
